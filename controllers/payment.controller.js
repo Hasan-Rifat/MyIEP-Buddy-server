634bcbcb8391ab6841681453d3c0cbaadbf1262b
hasan-rifat <developerhr000@gmail.com>
2023-05-16 01:16:35 +0600

    done may 16

--- controllers/payment.controller.js
+++ controllers/payment.controller.js
@@ -64,3 +64,28 @@ module.exports.getSingleUser = async (req, res) => {
     res.status(500).json({ error: "An error occurred" });
   }
 };
+
+module.exports.deleteSubscription = async (req, res) => {
+  try {
+    // Find the customer associated with the email
+    const customer = await stripe.customers.list({
+      email: req.params.email,
+      limit: 1, // Limit the result to 1 customer
+    });
+
+    if (customer.data.length === 0) {
+      // If no customer found with the provided email
+      return res.status(404).json({ error: "Customer not found" });
+    }
+
+    const customerId = customer.data[0].id;
+
+    // Delete the customer
+    await stripe.customers.del(customerId);
+
+    res.status(200).json({ message: "Subscriber deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting subscriber:", error);
+    res.status(500).json({ error: "An error occurred" });
+  }
+};
--- routers/v1/payment/payment.router.js
+++ routers/v1/payment/payment.router.js
@@ -3,10 +3,11 @@ const {
   subscriptionPayment,
   getAllUser,
   getSingleUser,
+  deleteSubscription,
 } = require("../../../controllers/payment.controller");
 const router = express.Router();
 
 router.route("/").get(getAllUser); //.post(subscriptionPayment);
-router.route("/:email").get(getSingleUser);
+router.route("/:email").get(getSingleUser).delete(deleteSubscription);
 
 module.exports = router;
